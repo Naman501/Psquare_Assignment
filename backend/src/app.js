a859d3c514994d0d57f13a9fe835d2ec1e301a60
Naman Mittal <namanmittal501@gmail.com>
2025-12-19 03:10:34 +0530

    Set up Express server with MongoDB connection and initial route

---
+++ backend/src/app.js
@@ -0,0 +1,35 @@
+// const express = require("express");
+// const cors = require("cors");
+
+// const authRoutes = require("./routes/authRoutes");
+// const companyRoutes = require("./routes/companyRoutes");
+// const jobRoutes = require("./routes/jobRoutes");
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res.send('API is running');
+// });
+
+
+// app.use("/api/auth", authRoutes);
+// app.use("/api/companies", companyRoutes);
+// app.use("/api/jobs", jobRoutes);
+
+// module.exports = app;
+const express = require("express");
+
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+module.exports = app;
