0b99f01ebbe5da6ba0211a8af2751cfbf80b5378
Naman Mittal <namanmittal501@gmail.com>
2026-01-02 00:54:38 +0530

    Brevo

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,10 +1,25 @@
+// const nodemailer = require("nodemailer");
+
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL,
+//     pass: process.env.EMAIL_PASS
+//   }
+// });
+
+// module.exports = transporter;
+
+
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
-    user: process.env.EMAIL,
-    pass: process.env.EMAIL_PASS
+    user: "apikey",
+    pass: process.env.BREVO_API_KEY
   }
 });
 
